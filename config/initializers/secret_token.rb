d3e31c559a39e95bd7bec94ba36d6c713b2e73a1
Andy Maleh <andy.am@gmail.com>
2012-11-07 17:14:49 -0600

    Initial Rails project commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HealthySwap::Application.config.secret_token = '2118a9a80bcc885153f94b37e98f506247cfa336a90dfdb897d8574171d3fad76cb6c9356c34e192cfe569019a61762a160bcd927299688eb270c4db2dfca091'
